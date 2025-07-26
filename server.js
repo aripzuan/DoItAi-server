75b26ff64cb12353e2fd43eb00ea75c4104f4136
aripzuan <arridz03@gmail.com>
2025-07-26 23:55:37 +0800

    api on generate article with gemini ai api

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 import express from 'express';
 import cors from 'cors';
 import 'dotenv/config';
-import { clerkMiddleware } from '@clerk/express'
+import { clerkMiddleware, requireAuth } from '@clerk/express'
+import aiRouter from './routes/aiRoutes.js';
 
 const app = express()
 
@@ -11,6 +12,10 @@ app.use(express.json())
 app.use(clerkMiddleware())
 
 app.get('/', (req,res)=> res.send('Server is running'))
+
+app.use(requireAuth())
+
+app.use('/api/ai', aiRouter)
 
 const PORT = process.env.PORT || 3000;
 

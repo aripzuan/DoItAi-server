a7b86ffdfdf9bc683b4e3b14659d40afb512bf90
aripzuan <arridz03@gmail.com>
2025-07-28 01:03:31 +0800

    connect to api

--- routes/aiRoutes.js
+++ routes/aiRoutes.js
@@ -1,9 +1,15 @@
 import express from "express";
 import { auth } from "../middlewares/Auth.js";
-import { generateArticle } from "../controllers/aiController.js";
+import { generateArticle, generateBlogTitle, generateImage, removeImageBackground, removeImageObject, resumeReview } from "../controllers/aiController.js";
+import { upload } from "../configs/multer.js";
 
 const aiRouter = express.Router();
 
-aiRouter.post("/generate-article", auth, generateArticle);
+aiRouter.post("/write-article", auth, generateArticle);
+aiRouter.post("/blog-titles", auth, generateBlogTitle);
+aiRouter.post("/generate-image", auth, generateImage);
+aiRouter.post("/remove-background", upload.single('image'), auth, removeImageBackground);
+aiRouter.post("/remove-object", upload.single('image'), auth, removeImageObject);
+aiRouter.post("/review-resume", upload.single('resume'), auth, resumeReview);
 
 export default aiRouter;
